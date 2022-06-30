87a001b1a2c31d53dd17a536ecf7a5207037d528
Emzzy241 <emzzyoluwole@gmail.com>
2022-06-30 21:10:35 +0100

    done with the javascript for search, now time for the shopping cart

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
-$(document).ready( function(){
-    
-});
+let searchForm = document.querySelector('.search-form');
+
+// showing the search window when we click on search
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+}
