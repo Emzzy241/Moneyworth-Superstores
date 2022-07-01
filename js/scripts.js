1cc591be773f5e43022a392186800dd44a549c5c
Emzzy241 <emzzyoluwole@gmail.com>
2022-07-01 20:07:21 +0100

    done with the products section

--- js/scripts.js
+++ js/scripts.js
@@ -56,3 +56,23 @@ window.onscroll = () => {
     navbar.classList.remove('active');
 }
 
+var swiper = new Swiper(".product-slider", {    
+    loop: true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+  });
