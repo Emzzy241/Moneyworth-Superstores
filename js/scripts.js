d6a40890ae9f095dcef93cbde7671b5700df5b46
Emzzy241 <emzzyoluwole@gmail.com>
2022-07-01 08:59:06 +0100

    done with the shopping cart section

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,14 @@ let searchForm = document.querySelector('.search-form');
 // showing the search window when we click on search
 
 document.querySelector('#search-btn').onclick = () =>{
-    searchForm.classList.toggle('active');
+    searchForm.classList.toggle('active'); 
 }
+
+
+let shoppingCart = document.querySelector('.shopping-cart');
+
+// showing the shopping cart window when we click on search
+
+document.querySelector('#cart-btn').onclick = () =>{
+    shoppingCart.classList.toggle('active'); 
+} 
