5f3534bad58d9800ca0478c919b0b7dcfb89b394
Emzzy241 <emzzyoluwole@gmail.com>
2022-07-02 09:21:05 +0100

    writing the customer's review section code

--- js/scripts.js
+++ js/scripts.js
@@ -76,3 +76,27 @@ var swiper = new Swiper(".product-slider", {
       },
     },
   });
+
+// Thse slider for the customer's review section 
+
+
+var swiper = new Swiper(".review-slider", {    
+    loop: true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+  });
