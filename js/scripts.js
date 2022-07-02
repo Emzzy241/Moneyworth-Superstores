67b8881453c02944d91783b8314b5bc426a12cfc
Emzzy241 <emzzyoluwole@gmail.com>
2022-07-02 19:28:19 +0100

    done with baby cereals

--- js/scripts.js
+++ js/scripts.js
@@ -100,3 +100,27 @@ var swiper = new Swiper(".review-slider", {
       },
     },
   });
+
+  
+// swiper for baby.html 
+  
+var swiper = new Swiper(".diaper-slider", {    
+    loop: true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+  });
